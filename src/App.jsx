1a6d0e4673a780b1e8a7dba4dd431a9337dae82b
pedro sky <pedroassis100@gmail.com>
2026-04-09 15:33:19 -0300

    more tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Estudar",
+      day: "10 de abril às 10:00",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Fazer compras",
+      day: "10 de abril às 15:00",
+      reminder: true,
+    },
+  ]);
+
   return (
-    <div>
-      <h1 className="text-red-500">Gerenciador de tarefas</h1>
-      <Tasks />
-      <AddTask />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de tarefas
+        </h1>
+        <Tasks />
+        <AddTask />
+      </div>
     </div>
   );
 }
-
-<div className="bg-blue-500"></div>;
 
 export default App;
