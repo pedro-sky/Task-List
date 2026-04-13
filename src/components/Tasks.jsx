888cdf66f5d954cd00e1770ce5758b9a6a1133b7
pedro sky <pedroassis100@gmail.com>
2026-04-13 18:44:36 -0300

    "delete" and "complete" functions added

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,20 +1,32 @@
-import { ChevronRightIcon } from "lucide-react";
+import {
+  ChevronRightIcon,
+  Delete,
+  DeleteIcon,
+  Trash2Icon,
+  TrashIcon,
+} from "lucide-react";
 
-function Tasks(props) {
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-      {props.tasks.map((tasks) => (
+      {tasks.map((tasks) => (
         <li key={tasks.id} className="flex gap-2">
           <button
-            onClick={() => props.onTaskClick(tasks.id)}
-            className="bg-slate-400 text-left w-full text-white p-2 rounded-md"
+            onClick={() => onTaskClick(tasks.id)}
+            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md
+              ${tasks.isCompleted && "line-through"}`}
           >
             {tasks.title}
-            {tasks.isCompleted ? "COMPLETE" : "INCOMPLETE"}
           </button>
 
           <button className="bg-slate-400 p-2 rounded-md text-white">
             <ChevronRightIcon />
+          </button>
+          <button
+            onClick={() => onDeleteTaskClick(tasks.id)}
+            className="bg-slate-400 p-2 rounded-md text-white"
+          >
+            <Trash2Icon />
           </button>
         </li>
       ))}
